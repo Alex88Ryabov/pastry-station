2524db52f0a65191e0ba3384a66165b320bce052
Alex <ryabovsasha88@gmail.com>
2021-11-16 15:14:57 +0200

    added products.json. added productCardAddData.

--- src/js/app.js
+++ src/js/app.js
@@ -1,52 +1,68 @@
-// import sayHello from './lib/sayHello';
-
-// sayHello();
 import JsTabs from 'js-tabs';
 import burgerToggle from './components/burgerToggle';
+import productCardAddData from './components/productCardAddData';
 
 
 burgerToggle();
 
+if (window.location.pathname === '/' || window.location.pathname === '/index.html') {
+  fetch('/data/categories.json')
+    .then(response => {
+      response.json().then(res => {
+        const categories = res;
+        const tabTitleTemplate = document.querySelector('.template-tab-title');
+        const tabContentTemplate = document.querySelector('.template-tab-content');
+        const tabTitles = document.querySelector('.js-tabs-head');
+        const tabContents = document.querySelector('.js-tabs-content');
 
-fetch('/data/categories.json')
-  .then(response => {
-    response.json().then(res => {
-      const categories = res;
-      const categoryTabsTemplate = document.querySelector('.template-category-tab-head');
-      const categoriesTabsHead = document.querySelector('.js-categories-tabs-head');
+        categories.forEach((category, i) => {
+          const tabTitle = tabTitleTemplate.content.cloneNode(true);
+          const tabContent = tabContentTemplate.content.cloneNode(true);
 
-      categories.forEach(category => {
-        const categoryTabsItem = categoryTabsTemplate.content.cloneNode(true);
+          if (i === 0) {
+            tabTitle.querySelector('li').className = `tabs__tab js-tabs__tab active`;
+            tabContent.querySelector('li').className = 'js-tabs__content active'
+            productCardAddData(tabContent.querySelector('li'), category.id);
+          } else {
+            tabTitle.querySelector('li').className = `tabs__tab js-tabs__tab`;
+            tabContent.querySelector('li').className = 'js-tabs__content';
+            productCardAddData(tabContent.querySelector('li'), category.id);
+          }
 
-        categoryTabsItem.querySelector('li').className = `tabs__tab js-tabs__tab ${category.class}`;
-        categoryTabsItem.querySelector('img').src = category.imageUrl;
-        categoryTabsItem.querySelector('.js-tab-title').textContent = category.name;
-        categoriesTabsHead.appendChild(categoryTabsItem);
-      });
+          tabTitle.querySelector('img').src = category.imageUrl;
+          tabTitle.querySelector('.js-tab-title').textContent = category.name;
 
-      if (window.location.pathname === '/' || window.location.pathname === '/index.html') {
+          tabTitles.appendChild(tabTitle);
+          tabContents.appendChild(tabContent)
+        });
+
         const myTabs = new JsTabs({
           elm: '#my-js-tabs'
         })
         myTabs.init();
-      };
-
+      })
     })
-  })
-  .catch(error => {
-    console.log(error);
-  });
+    .catch(error => {
+      console.log(error);
+    });
+}
+// const tabTitles = document.querySelectorAll('.js-tabs__tab')
+
+// tabTitles.forEach(tabTitle => {
+//   tabTitle.addEventListener('click', (e) => {
+//     console.log(e.taget);
+//   })
+// })
+
+const queryParams = window.location.search.slice(window.location.search.indexOf('?') + 1).split('&').reduce((acc, query) => {
+  const parts = query.split('=');
+  acc[parts[0]] = parts[1] ? decodeURIComponent(parts[1]) : parts[1];
+  return acc;
+}, {});
 
 fetch('/data/products.json')
-  .then(response => {
-    response.json().then(res => {
-      console.log(res);
+  .then(responce => {
+    responce.json().then(res => {
+      console.log(res.find(p => p.id === +queryParams.id));
     })
   })
-  .catch(error => {
-    console.log(error);
-  })
-
-
-
-
---
+++ src/js/components/productCardAddData.js
@@ -0,0 +1,36 @@
+function createProducts(products, tabContent) {
+    const productsTemplate = document.querySelector('.template-product-card');
+
+    products.forEach(product => {
+        const productItem = productsTemplate.content.cloneNode(true);
+        productItem.querySelector('li').className = `product`;
+        productItem.querySelector('a').href = `${product.link}?id=${product.id}`;
+        productItem.querySelector('.js-product__img').src = product.imgUrl;
+        productItem.querySelector('.js-product__img').alt = product.title;
+        productItem.querySelector('.js-product__type').textContent = product.type;
+        productItem.querySelector('.js-product__icon').src = product.icon;
+        productItem.querySelector('.js-product__icon').alt = product.iconName;
+        productItem.querySelector('h4').textContent = product.title;
+        productItem.querySelector('.js-product__price').textContent = product.price;
+
+        tabContent.appendChild(productItem);
+    });
+}
+
+function productCardAddData(tabContent, parentId) {
+
+    fetch('/data/products.json')
+        .then(response => {
+            response.json().then(res => {
+                const products = res.filter(product => product.parentId === parentId);
+                createProducts(products, tabContent)
+            })
+        })
+        .catch(error => {
+            console.log(error);
+        })
+
+};
+
+module.exports = productCardAddData;
+
